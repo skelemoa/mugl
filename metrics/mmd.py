8d9cf46aad74f8a9ce905ef9ef1398832b82056c
Debtanu <debtanugupta177@gmail.com>
2022-01-01 23:30:01 +0530

    update evaluation script

--- metrics/mmd.py
+++ metrics/mmd.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+
+
 def rkhs_mmd(samples_1, samples_2, bandwidth):  # two given sample groups, shape (N*dim)
 	m, dim = np.shape(samples_1)
 	n, _ = np.shape(samples_2)
@@ -19,13 +21,13 @@ def rkhs_mmd(samples_1, samples_2, bandwidth):  # two given sample groups, shape
 
 
 
-def calculate_mmd(sequence_1, sequence_2, bandwidth, mode='MMD-A'):  # compute the mmd between sequences, shape (N*len*dim)
+def calculate_mmd(sequence_1, sequence_2, bandwidth, mode='MMDA'):  # compute the mmd between sequences, shape (N*len*dim)
     _, seq_len, dim = np.shape(sequence_1)
     result = 0.
-    if mode == 'MMD-A':
+    if mode == 'MMDA':
         for frames in range(seq_len):
             result += rkhs_mmd(sequence_1[:, frames, :], sequence_2[:, frames, :], bandwidth)/seq_len
-    elif mode == 'MMD-S':
+    elif mode == 'MMDS':
         flat_seq_1 = np.reshape(sequence_1, (-1, dim*seq_len))
         flat_seq_2 = np.reshape(sequence_2, (-1, dim*seq_len))
         result = rkhs_mmd(flat_seq_1, flat_seq_2, bandwidth)
@@ -34,14 +36,15 @@ def calculate_mmd(sequence_1, sequence_2, bandwidth, mode='MMD-A'):  # compute t
     return result
 
 
-def mmd_function(gen, real, y):
+def mmd_function(gen, real, y, mode='MMDA'):
 	labels = np.unique(y)
 	new_r = 0
 	result_list = []
 	for j in range(-4,10):
-		new_new_r = calculate_mmd(gen, real, 10 ** j)
+		new_new_r = calculate_mmd(gen, real, 10 ** j, mode=mmd_mode)
 		if new_new_r > new_r:
 			new_r = new_new_r
 		result_list.append(new_r)
 	result = np.mean(result_list)
 	return result
+
